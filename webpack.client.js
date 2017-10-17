fa32e8c001f95915767808cf1f8716d7fe3689b4
Vladimir Jelicic <vladimir.jelincic@gmail.com>
2017-10-17 21:20:32 +0200

    Redux added

--- webpack.client.js
+++ webpack.client.js
@@ -9,6 +9,7 @@ const config = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
-  }
+  },
+  devtool: 'source-map'
 };
 module.exports = merge(baseConfig, config);
